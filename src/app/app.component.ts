250434de648408bdef253aee9a5994408c2c3d6a
Ayoola Taiwo <macbrill13@gmail.com>
2021-05-01 09:18:41 +0100

    initiated angular app for filrd sssessmrnt project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'filed-frontend-assessment';
+  title: string = 'filed-frontend-assessment';
+  name: string = 'Ayoola Taiwo'
 }
